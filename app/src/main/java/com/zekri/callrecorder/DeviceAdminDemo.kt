e10ff06d7b3412100b1d646c7ac2bca59875f1ee
ahmed103 <ahmed.zekri.a@gmail.com>
2022-06-26 21:40:44 +0100

    fixed component name

--- app/src/main/java/com/zekri/callrecorder/DeviceAdminDemo.kt
+++ app/src/main/java/com/zekri/callrecorder/DeviceAdminDemo.kt
@@ -13,15 +13,17 @@ class DeviceAdminDemo : DeviceAdminReceiver() {
 
     }
 
-    override fun onEnabled(context: Context, intent: Intent) =
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
         showToast(context, context.getString(R.string.admin_receiver_status_enabled))
+    }
 
     override fun onDisableRequested(context: Context, intent: Intent): CharSequence =
         context.getString(R.string.admin_receiver_status_disable_warning)
 
-    override fun onDisabled(context: Context, intent: Intent) =
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
         showToast(context, context.getString(R.string.admin_receiver_status_disabled))
-
-
+    }
 
 }
--- app/src/main/java/com/zekri/callrecorder/MainActivity.kt
+++ app/src/main/java/com/zekri/callrecorder/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.zekri.callrecorder
 
+import android.app.admin.DeviceAdminReceiver
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.zekri.callrecorder.presentation.call.CallScreen
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -10,7 +17,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val componentName = ComponentName(this, DeviceAdminDemo::class.java)
+        val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName)
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Example")
+        startActivity(intent)
+        setContent {
 
+            CallScreen()
+
+        }
 
     }
 
